44e0cefcf577f29cecda7e6c99394ec26fb37cd4
Jan <krimdomu@googlemail.com>
2011-11-27 12:44:04 +0100

    added listing of files

--- lib/Stashy/Rex.pm
+++ lib/Stashy/Rex.pm
@@ -3,19 +3,34 @@ use strict;
 use warnings;
 
 use Mojo::Base 'Mojolicious::Controller';
-use Cwd qw(getcwd);
+use Cwd qw(getcwd realpath);
 
 sub orders {
    my $self = shift;
-   $self->render_text(__list_orders_dir());
+   my $file = $self->param("file");
+
+   if($file) {
+      my $file_to_read = realpath(getcwd() . "/" . $self->stash->{"config"}->{"scm"}->{"checkout_to"} . "/" . $file);
+      my $__config_scm_checkout_to = realpath(getcwd() . "/" . $self->stash->{"config"}->{"scm"}->{"checkout_to"});
+      if($file_to_read =~ /^$__config_scm_checkout_to/ && -f $file_to_read) {
+         my $file_content = eval { local(@ARGV, $/) = ($file_to_read); <>; };
+         return $self->render_data($file_content);
+      }
+
+   }
+
+   # always fall back to file listing
+   $self->render_text($self->__list_orders_dir());
 }
 
 
 sub __list_orders_dir {
+   my $self = shift;
+
    my $ret = "";
 
    my $pwd = getcwd;
-   chdir($Stashy::SCM_CHECKOUT_PATH);
+   chdir($self->stash->{"config"}->{"scm"}->{"checkout_to"});
    my @dirs = (".");
 
    for my $dir (@dirs) {
